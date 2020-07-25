eb54fc7e97e6d47a31ae7f053103bab216058dae
odunayo12 <odun.rotimi@gmail.com>
2020-07-25 16:15:25 +0200

    format code

--- assets/segmentation_2019.R
+++ assets/segmentation_2019.R
@@ -1,5 +1,6 @@
-data_pbi_2019 <- read_csv("Data/Raw/data_pbi_2019.csv",
-
+data_pbi_2019 <- read_csv(
+  "Data/Raw/data_pbi_2019.csv",
+  
   col_types =  cols(
     Id = col_character(),
     Name = col_character(),
@@ -12,45 +13,78 @@ data_pbi_2019 <- read_csv("Data/Raw/data_pbi_2019.csv",
     Data.Column6 = col_character(),
     Data.Column7 = col_character(),
     Data.Column8 = col_character()
-  ))
+  )
+)
 View(data_pbi_2019)
 
 
-data_pbi_2019_start <- data_pbi_2019 %>% 
+data_pbi_2019_start <- data_pbi_2019 %>%
   arrange(Id) %>%
-  mutate(subCostCenterSum_Code = case_when(
-    #dectects any figure from 0-9 in a column
-    str_detect(Data.Column2, "[0-9]") &
-      str_length(Data.Column2) == 9 ~ paste0('0', Data.Column2)
-  ),
-  #the following detects the apperance of "ERGP" strictly () once {1}
-  projectCode = case_when(str_detect(Data.Column1, "^ERGP{1}") ~ Data.Column1),
-  expenditureCods = case_when(
-    #detects "^2" from the beginning of the column content
-    str_detect(Data.Column1, "^2") &
-      #[A-Z] because the desired colum have text in the next column
-      str_detect(Data.Column2, "[A-Z]") ~ Data.Column1),
-  #here we choose to identify relevant tables by their starting codes which in most cases only have two columns thus the sucessuive columns will be empty
-  # !is.na(projectCode) ensures that table identifier does not include the projects. we will fix it in the comprehensive table
-  table_identifier = case_when(str_detect(Data.Column1, "^[0-9]") & is.na(Data.Column3) & is.na(Data.Column4) & is.na(Data.Column5) & !is.na(projectCode)
-                               ~ paste0('0', Data.Column1)),
-  table_identifier_MDA = case_when(!is.na(table_identifier) ~ Data.Column2)
+  mutate(
+    subCostCenterSum_Code = case_when(
+      #dectects any figure from 0-9 in a column
+      str_detect(Data.Column2, "[0-9]") &
+        str_length(Data.Column2) == 9 ~ paste0('0', Data.Column2)
+    ),
+    #the following detects the apperance of "ERGP" strictly () once {1}
+    projectCode = case_when(str_detect(Data.Column1, "^ERGP{1}") ~ Data.Column1),
+    expenditureCods = case_when(
+      #detects "^2" from the beginning of the column content
+      str_detect(Data.Column1, "^2") &
+        #[A-Z] because the desired colum have text in the next column
+        str_detect(Data.Column2, "[A-Z]") ~ Data.Column1
+    ),
+    #here we choose to identify relevant tables by their starting codes which in most cases only have two columns thus the sucessuive columns will be empty
+    # !is.na(projectCode) ensures that table identifier does not include the projects. we will fix it in the comprehensive table
+    table_identifier = case_when(
+      str_detect(Data.Column1, "^[0-9]") &
+        is.na(Data.Column3) &
+        is.na(Data.Column4) & is.na(Data.Column5) & !is.na(projectCode)
+      ~ paste0('0', Data.Column1)
+    ),
+    table_identifier_MDA = case_when(!is.na(table_identifier) ~ Data.Column2)
   ) %>%
   # we then fill the sucessive rows downwards
-  #fill(table_identifier, table_identifier_MDA) %>% 
-  mutate( costCenter_Code = str_sub(table_identifier, end = 4),
-         lineExpCode = case_when(Data.Column1 == "2" & str_detect(Data.Column2, "^EXP{1}") ~ Data.Column1),
-         lineExpTerm = case_when(!is.na(lineExpCode) ~ Data.Column2), 
-         lineExpCodeLevel1 = case_when(str_detect(Data.Column1, "21|22|23|24") & str_length(Data.Column1) == 2 & str_length(Data.Column2) > 5~ Data.Column1),
-         lineExpTermLevel1 = case_when(!is.na(lineExpCodeLevel1) ~ Data.Column2), 
-         lineExpCodeLevel2 = case_when(str_detect(Data.Column1, "21|22|23|24") & str_length(Data.Column1) == 4 ~ Data.Column1),
-         lineExpTermLevel2 = case_when(!is.na(lineExpCodeLevel2) ~ Data.Column2),
-         lineExpCodeLevel3 = case_when(str_detect(Data.Column1, "21|22|23|24") & str_length(Data.Column1) == 6 ~ Data.Column1),
-         lineExpTermLevel3 = case_when(!is.na(lineExpCodeLevel3) ~ Data.Column2),
-         lineExpCodeLevel4 = case_when(str_detect(Data.Column1, "21|22|23|24") & str_length(Data.Column1) == 8 ~ Data.Column1),
-         lineExpTermLevel4 = case_when(!is.na(lineExpCodeLevel4) ~ Data.Column2),
-         Year = 2019
-  ) %>% 
-  #arrange(Id, Data.Column1) %>% 
-  fill(lineExpCode, lineExpTerm, lineExpCodeLevel1, lineExpTermLevel1, lineExpTermLevel2, lineExpCodeLevel2, lineExpTermLevel3, lineExpCodeLevel3) %>% 
+  #fill(table_identifier, table_identifier_MDA) %>%
+  mutate(
+    costCenter_Code = str_sub(table_identifier, end = 4),
+    lineExpCode = case_when(
+      Data.Column1 == "2" &
+        str_detect(Data.Column2, "^EXP{1}") ~ Data.Column1
+    ),
+    lineExpTerm = case_when(!is.na(lineExpCode) ~ Data.Column2),
+    lineExpCodeLevel1 = case_when(
+      str_detect(Data.Column1, "21|22|23|24") &
+        str_length(Data.Column1) == 2 &
+        str_length(Data.Column2) > 5 ~ Data.Column1
+    ),
+    lineExpTermLevel1 = case_when(!is.na(lineExpCodeLevel1) ~ Data.Column2),
+    lineExpCodeLevel2 = case_when(
+      str_detect(Data.Column1, "21|22|23|24") &
+        str_length(Data.Column1) == 4 ~ Data.Column1
+    ),
+    lineExpTermLevel2 = case_when(!is.na(lineExpCodeLevel2) ~ Data.Column2),
+    lineExpCodeLevel3 = case_when(
+      str_detect(Data.Column1, "21|22|23|24") &
+        str_length(Data.Column1) == 6 ~ Data.Column1
+    ),
+    lineExpTermLevel3 = case_when(!is.na(lineExpCodeLevel3) ~ Data.Column2),
+    lineExpCodeLevel4 = case_when(
+      str_detect(Data.Column1, "21|22|23|24") &
+        str_length(Data.Column1) == 8 ~ Data.Column1
+    ),
+    lineExpTermLevel4 = case_when(!is.na(lineExpCodeLevel4) ~ Data.Column2),
+    Year = 2019
+  ) %>%
+  #arrange(Id, Data.Column1) %>%
+  fill(
+    lineExpCode,
+    lineExpTerm,
+    lineExpCodeLevel1,
+    lineExpTermLevel1,
+    lineExpTermLevel2,
+    lineExpCodeLevel2,
+    lineExpTermLevel3,
+    lineExpCodeLevel3
+  ) %>%
   select(-Name, -Kind)
--- assets/segmentation_merge.R
+++ assets/segmentation_merge.R
@@ -1,29 +1,52 @@
-budget_18_19_20 <- rbind(data_pbi_2018, data_pbi_2019_start, data_pbi_2020_start)
+budget_18_19_20 <-
+  rbind(data_pbi_2018, data_pbi_2019_start, data_pbi_2020_start)
 
 
-projects_Unique <- as_tibble(budget_18_19_20) %>% 
-  filter(!is.na(projectCode)) %>% 
-  select(table_identifier, table_identifier_MDA, costCenter_Code, Year, projectCode,  Data.Column2, Data.Column3, Data.Column4) %>% 
-  rename(project_description = Data.Column2, Status = Data.Column3, Amount = Data.Column4) %>% 
-  filter( Year != 2019) %>% 
-  mutate(Amount = as.numeric(str_replace_all(Amount,",",""))) %>% 
+projects_Unique <- as_tibble(budget_18_19_20) %>%
+  filter(!is.na(projectCode)) %>%
+  select(
+    table_identifier,
+    table_identifier_MDA,
+    costCenter_Code,
+    Year,
+    projectCode,
+    Data.Column2,
+    Data.Column3,
+    Data.Column4
+  ) %>%
+  rename(project_description = Data.Column2,
+         Status = Data.Column3,
+         Amount = Data.Column4) %>%
+  filter(Year != 2019) %>%
+  mutate(Amount = as.numeric(str_replace_all(Amount, ",", ""))) %>%
   #filter(table_identifier_MDA == "OFFICE OF THE CHIEF OF STAFF TO THE PRESIDENT")
-  # group_by(Year, table_identifier, table_identifier_MDA) %>% 
+  # group_by(Year, table_identifier, table_identifier_MDA) %>%
   # summarise(count = n()) OFFICE OF THE CHIEF OF STAFF TO THE PRESIDENT
   # across() selects specified columns
   distinct(projectCode, across(table_identifier:Status))
-  
-  
-  
-projects_2019 <- as_tibble(budget_18_19_20) %>% 
-  filter(!is.na(projectCode)) %>% 
-  select(table_identifier, table_identifier_MDA, costCenter_Code, Year, projectCode,  Data.Column2, Data.Column3, Data.Column4) %>% 
-  rename(project_description = Data.Column2, Status = Data.Column3, Amount = Data.Column4) %>% 
-  filter( Year == 2019) %>% 
-  mutate(Amount = as.numeric(str_replace_all(Amount,",",""))) %>% 
-  left_join(projects_Unique, by= "projectCode")  %>% 
+
+
+
+projects_2019 <- as_tibble(budget_18_19_20) %>%
+  filter(!is.na(projectCode)) %>%
+  select(
+    table_identifier,
+    table_identifier_MDA,
+    costCenter_Code,
+    Year,
+    projectCode,
+    Data.Column2,
+    Data.Column3,
+    Data.Column4
+  ) %>%
+  rename(project_description = Data.Column2,
+         Status = Data.Column3,
+         Amount = Data.Column4) %>%
+  filter(Year == 2019) %>%
+  mutate(Amount = as.numeric(str_replace_all(Amount, ",", ""))) %>%
+  left_join(projects_Unique, by = "projectCode")  %>%
   distinct(projectCode, .keep_all = TRUE)
-    #filter(table_identifier_MDA == "OFFICE OF THE CHIEF OF STAFF TO THE PRESIDENT")
-    # group_by(Year, table_identifier, table_identifier_MDA) %>% 
-    # summarise(count = n()) OFFICE OF THE CHIEF OF STAFF TO THE PRESIDENT
-    #distinct(projectCode, across())
+#filter(table_identifier_MDA == "OFFICE OF THE CHIEF OF STAFF TO THE PRESIDENT")
+# group_by(Year, table_identifier, table_identifier_MDA) %>%
+# summarise(count = n()) OFFICE OF THE CHIEF OF STAFF TO THE PRESIDENT
+#distinct(projectCode, across())
