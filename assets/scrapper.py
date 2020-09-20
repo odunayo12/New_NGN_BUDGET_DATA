fe4fddb2a889b199e2af3f77741f91cd99d58341
odunayo12 <odun.rotimi@gmail.com>
2020-09-20 16:55:24 +0200

    2014 scrapped

--- assets/scrapper.py
+++ assets/scrapper.py
@@ -32,13 +32,22 @@ for each_item in limit:
             for h3_list in each_list.findAll("a"):
                 anchor_tag = h3_list.get('href')
                 regex = re.compile(r'[\n\r\t/, ]')
-                pdf_title = regex.sub("", h3_list.text) + '.pdf'
+                pdf_title_re = regex.sub("", h3_list.text) + '.pdf'
+                if re.search('RevenueM', pdf_title_re):
+                    pdf_title_re = ""
+                    # soup_box.append()
+                    #print(re.search('RevenueM', pdf_title_re).group())
+                else:
+                    pdf_title = pdf_title_re
+                    # print(pdf_title)
+                    # print(len(pdf_title))
                 # get the last 8 characters of the anchor tag
                 if anchor_tag[23] is not "r" and anchor_tag[-8:] == "download":
                     lead_domain = "https://www.budgetoffice.gov.ng"
                     # soup_box.append(lead_domain + anchor_tag)
                     pdf_file_dwnld = lead_domain + anchor_tag
-                    soup_box.append(pdf_file_dwnld)
+                    #anchor_tag = h3_list.get('href')
+                    soup_box.append(pdf_file_dwnld + pdf_title)
                 # directories
                 fileDir = os.path.abspath(os.path.join(
                     os.path.dirname('__file__'), '..', 'scrapped-files'))
@@ -47,31 +56,65 @@ for each_item in limit:
                 pdf_file = open(filepath, "wb")
                 pdf_file.write(urllib.request.urlopen(pdf_file_dwnld).read())
                 pdf_file.close()
-                # directories
-                import_files_dir = os.path.join(fileDir, '*.pdf')
-                import_files = [
-                    folder_content for folder_content in glob.glob(import_files_dir)]
-                # convert to csv
-                for pdf_filepath in import_files:
-                    csv_filepath = pdf_filepath.replace('.pdf', '.csv')
-                    tabula.convert_into(pdf_filepath, csv_filepath,
-                                        lattice=True,  output_format="csv", pages="all")
-
 # %%
-soup_box = []
-years_s = [2014, 2015, 2016]
-limit = [0, 15, 30, 45]
+# directories
+import_files_dir = os.path.join(fileDir, '*.pdf')
+import_files = [
+    folder_content for folder_content in glob.glob(import_files_dir)]
+# convert to csv
+for pdf_filepath in import_files:
+    csv_filepath = pdf_filepath.replace('.pdf', '.csv')
+    tabula.convert_into(pdf_filepath, csv_filepath,
+                        lattice=True,  output_format="csv", pages="all")
 
-for each_stuff in years_s:
-    for each_item in limit:
-        soup = make_soup(
-            f'https://www.budgetoffice.gov.ng/index.php/{each_stuff}-budget?start={each_item}')
-        for target_list in soup.findAll("div", {"id": "edocman-documents"}):
-            for each_list in target_list.findAll("h3"):
-                for h3_list in each_list.findAll("a"):
-                    anchor_tag = h3_list.get('href')
-                    regex = re.compile(r'[\n\r\t/, ]')
-                    pdf_title = regex.sub(
-                        "", h3_list.text) + f'{each_stuff}.pdf'
+# # %%
+# soup_bowl = []
+# years_s = [2014, 2015, 2016]
+# limit = [0, 15, 30, 45]
 
-# %%
+# for each_stuff in years_s:
+#     for each_item in limit:
+#         soup = make_soup(
+#             f'https://www.budgetoffice.gov.ng/index.php/{each_stuff}-budget?start={each_item}')
+#         for target_list in soup.findAll("div", {"id": "edocman-documents"}):
+#             for each_list in target_list.findAll("h3"):
+#                 for h3_list in each_list.findAll("a"):
+#                     anchor_tag = h3_list.get('href')
+#                     regex = re.compile(r'[\n\r\t/, ]')
+#                     pdf_title = regex.sub(
+#                         "", h3_list.text) + f'{each_stuff}.pdf'
+#                     #soup_bowl.append(pdf_title)
+#                     if re.search('RevenueM', pdf_title_re):
+#                         pdf_title_re = ""
+#                         # soup_box.append()
+#                         #print(re.search('RevenueM', pdf_title_re).group())
+#                     else:
+#                         pdf_title = pdf_title_re
+#                         # print(pdf_title)
+#                         # print(len(pdf_title))
+#                     # get the last 8 characters of the anchor tag
+#                     if anchor_tag[23] is not "r" and anchor_tag[-8:] == "download":
+#                         lead_domain = "https://www.budgetoffice.gov.ng"
+#                         # soup_box.append(lead_domain + anchor_tag)
+#                         pdf_file_dwnld = lead_domain + anchor_tag
+#                         #anchor_tag = h3_list.get('href')
+#                         soup_box.append(pdf_file_dwnld + pdf_title)
+#                     # directories
+#                     fileDir = os.path.abspath(os.path.join(
+#                     os.path.dirname('__file__'), '..', 'scrapped-files'))
+#                     filepath = os.path.join(fileDir, pdf_title)
+#                     # write to file
+#                     pdf_file = open(filepath, "wb")
+#                     pdf_file.write(urllib.request.urlopen(pdf_file_dwnld).read())
+#                     pdf_file.close()
+
+# # %%
+# # directories
+# import_files_dir = os.path.join(fileDir, '*.pdf')
+# import_files = [
+#     folder_content for folder_content in glob.glob(import_files_dir)]
+# for each_stuff in years_s:
+#     for pdf_filepath in import_files:
+#         csv_filepath = os.path.join(pdf_filepath, f'{each_stuff}').replace('.pdf', '.csv')
+#         tabula.convert_into(pdf_filepath, csv_filepath,
+#                         lattice=True,  output_format="csv", pages="all")
