eb1ae9fc87f819e185e903c687d201fe83e76c09
odunayo12 <odun.rotimi@gmail.com>
2020-09-26 19:13:32 +0200

    On merging all years

--- assets/scrapper.py
+++ assets/scrapper.py
@@ -140,59 +140,106 @@ for each_item in limit:
 soup_bowl = []
 years_s = [2014, 2015, 2016, 2017]
 limit = [0, 15, 30, 45]
-
-for each_stuff in years_s:
+soup_box = []
+text_box = []
+anchor_tag_box = []
+for each_year in years_s:
     for each_item in limit:
-        if each_stuff != 2017:
+        if each_year != 2017:
             soup = make_soup(
-                f'https://www.budgetoffice.gov.ng/index.php/{each_stuff}-budget?start={each_item}')
-        else:
-            soup_2 = make_soup(
-                f'https://www.budgetoffice.gov.ng/index.php/{each_stuff}-approved-budget?start={each_item}')
-            soup.append(copy.copy(soup_2))
-            print(soup.prettify())
-        # soup = make_soup(
-        #     f'https://www.budgetoffice.gov.ng/index.php/{each_stuff}-budget?start={each_item}')
-        # for target_list in soup.findAll("div", {"id": "edocman-documents"}):
-        #     for each_list in target_list.findAll("h3"):
-        #         for h3_list in each_list.findAll("a"):
-        #             anchor_tag = h3_list.get('href')
-        #             regex = re.compile(r'[\n\r\t/, ]')
-        #             pdf_title = regex.sub(
-        #                 "", h3_list.text) + f'{each_stuff}.pdf'
-        #             #soup_bowl.append(pdf_title)
-        #             if re.search('RevenueM', pdf_title_re):
-        #                 pdf_title_re = ""
-        #                 # soup_box.append()
-        #                 #print(re.search('RevenueM', pdf_title_re).group())
-        #             else:
-        #                 pdf_title = pdf_title_re
-        #                 # print(pdf_title)
-        #                 # print(len(pdf_title))
-        #             # get the last 8 characters of the anchor tag
-        #             if anchor_tag[23] is not "r" and anchor_tag[-8:] == "download":
-        #                 lead_domain = "https://www.budgetoffice.gov.ng"
-        #                 # soup_box.append(lead_domain + anchor_tag)
-        #                 pdf_file_dwnld = lead_domain + anchor_tag
-        #                 #anchor_tag = h3_list.get('href')
-        #                 soup_box.append(pdf_file_dwnld + pdf_title)
-        #             # directories
-        #             fileDir = os.path.abspath(os.path.join(
-        #             os.path.dirname('__file__'), '..', 'scrapped-files'))
-        #             filepath = os.path.join(fileDir, pdf_title)
-        #             # write to file
-        #             pdf_file = open(filepath, "wb")
-        #             pdf_file.write(urllib.request.urlopen(pdf_file_dwnld).read())
-        #             pdf_file.close()
+                f'https://www.budgetoffice.gov.ng/index.php/{each_year}-budget?start={each_item}')
+            for target_list in soup.findAll("div", {"id": "edocman-documents"}):
+                for each_list in target_list.findAll("h3"):
+                    for h3_list in each_list.findAll("a"):
+                        anchor_tag = h3_list.get('href')
+                        html_text = h3_list.text
+                        anchor_tag_year = anchor_tag[11:15]
+                        soup_bowl.append(anchor_tag_year)
+                        regex = re.compile(r'[\n\r\t/, ]')
+                        pdf_title_re = (
+                            regex.sub("", html_text) + f'_{anchor_tag_year}.pdf')
+                        # print(pdf_title_re)
+
+                        text_box.append(pdf_title_re)
+                        anchor_tag_box.append(anchor_tag)
+
+                        # if re.findall(r'(?<=Rev|Sta)[a-zA-Z]+(e|n)2014|(?<=2014)\w+|(?<=2015|2016)App|Total|BudgetandtheStrategicI', str(pdf_title_re)):
+                        #     pdf_title_re = ""
+                        # else:
+                        #     pdf_title = pdf_title_re
+                        #     soup_box.append(pdf_title)
+
+                        # if re.findall(r'(?<=/index.php/(2014|2015|2016)-budget/)overv|prices|approp|analysis|consol', str(anchor_tag)):
+                        #     anchor_tag = ""
+                        # else:
+                        #     anchor_tag_new = anchor_tag
+                        #     lead_domain = "https://www.budgetoffice.gov.ng"
+                        #     pdf_file_dwnld = lead_domain + anchor_tag_new
+                        #     anchor_tag_box.append(pdf_file_dwnld)
+        # elif each_year==2017:
+        #     soup = make_soup(
+        #         f'https://www.budgetoffice.gov.ng/index.php/{each_year}-approved-budget?start={each_item}')
+        #     #soup.append(copy.copy(soup_2))
+        #     #soup_box.append(soup.append(copy.copy(soup_2)))
+        #     for target_list in soup.findAll("div", {"id": "edocman-documents"}):
+        #         for each_list in target_list.findAll("h3"):
+        #             for h3_list in each_list.findAll("a"):
+        #                 anchor_tag = h3_list.get('href')
+        #                 html_text = h3_list.text
+        #                 anchor_tag_year = anchor_tag[11:15]
+        #                 print(anchor_tag_year)
+        #                 soup_bowl.append(anchor_tag_year)
+                        # for each_tag in anchor_tag:
+                        #     anchor_tag_year = each_tag[11:15]
+                        #     print(anchor_tag_year)
+                        # regex = re.compile(r'[\n\r\t/, ]')
+                        # pdf_title_re = (
+                        #     regex.sub("", html_text) + f'{anchor_tag_year}.pdf')
+                        # # if each_year == 2017 or each_year == 2014:
+                        # #     pdf_title_re = (
+                        # #         regex.sub("", html_text) + f'{year_in}.pdf')
+                        # # else:
+                        # #     pdf_title_re = (
+                        # #         regex.sub("", html_text) + '.pdf')
+                        # #edocman-documents > div:nth-child(1) > div:nth-child(1) > div.edocman-box-heading.clearfix > h3 > a
+                        # #edocman-documents > div:nth-child(1) > div:nth-child(2) > div.edocman-box-heading.clearfix > h3 > a
+                        # #edocman-category > h1 > i
+                        # text_box.append(pdf_title_re)
+                        # anchor_tag_box.append(anchor_tag)
+
+                        # lead_domain = "https://www.budgetoffice.gov.ng"
+                        # pdf_file_dwnld = lead_domain + anchor_tag
+                        # # if re.findall(r'(Prices|^2015|Over|Consolidated)', str(pdf_title_re)):
+                        # #     pdf_title_re = ""
+                        # # else:
+                        # #     pdf_title = pdf_title_re
+                        # #     soup_box.append(pdf_title_re)
+
+                        # # if re.findall(r'(-analysis/|/consol|t/over|/2015-a|t/prices)', str(anchor_tag_box)):
+                        # #     anchor_tag = ""
+                        # # else:
+                        # #     anchor_tag_new = anchor_tag
+                        # #     lead_domain = "https://www.budgetoffice.gov.ng"
+                        # #     pdf_file_dwnld = lead_domain + anchor_tag_new
+                        # #     anchor_tag_box.append(pdf_file_dwnld)
+                        # # Directories
+                        # fileDir = os.path.abspath(os.path.join(
+                        #     os.path.dirname('__file__'), '..', 'scrapped-files'))
+                        # filepath = os.path.join(fileDir, pdf_title_re)
+                        # # write to file
+                        # pdf_file = open(filepath, "wb")
+                        # pdf_file.write(urllib.request.urlopen(pdf_file_dwnld).read())
+                        # pdf_file.close()
+
 
 # # %%
 # # directories
 # import_files_dir = os.path.join(fileDir, '*.pdf')
 # import_files = [
 #     folder_content for folder_content in glob.glob(import_files_dir)]
-# for each_stuff in years_s:
+# for each_year in years_s:
 #     for pdf_filepath in import_files:
-#         csv_filepath = os.path.join(pdf_filepath, f'{each_stuff}').replace('.pdf', '.csv')
+#         csv_filepath = os.path.join(pdf_filepath, f'{each_year}').replace('.pdf', '.csv')
 #         tabula.convert_into(pdf_filepath, csv_filepath,
 #                         lattice=True,  output_format="csv", pages="all")
 
@@ -204,14 +251,28 @@ for each_stuff in years_s:
 # new_list = re.sub(r, '', str(soup_box))
 # print(new_list)
 # %%
+# 2014
+# ^(RevenueM|Statutory)[a-zA-Z]2014.pdf$
+newlist = re.findall(
+    r'(?<=Rev|Sta)[a-zA-Z]+(e|n)2014|(?<=2014)\w+|(?<=2015|2016)App|Total|BudgetandtheStrategicI', str(text_box))
+# list(filter(r.match, str(anchor_tag_box)))
+print(len(newlist))
+print(newlist)
 
+
+newlist_2 = re.findall(
+    r'(?<=/index.php/(2014|2015|2016)-budget/)overv|prices|approp|analysis|consol', str(anchor_tag_box))
+# list(filter(r.match, str(anchor_tag_box)))
+print(len(newlist_2))
+print(newlist_2)
+# %%
 # 2015
 # r = re.compile(r'(service/|/consol|t/over)')
 # if re.findall(r'(Prices|^2015|Over|Consolidated)', str(pdf_title_re)):
-newlist = re.findall(
-    r'(-analysis/|/consol|t/over|/2015-a|t/prices)', str(anchor_tag_box))
-# list(filter(r.match, str(anchor_tag_box)))
-print(newlist)
+# newlist = re.findall(
+#     r'(-analysis/|/consol|t/over|/2015-a|t/prices)', str(anchor_tag_box))
+# # list(filter(r.match, str(anchor_tag_box)))
+# print(newlist)
 # %%
 # 2106
 newlist = re.findall(
@@ -222,6 +283,18 @@ print(newlist)
 
 newlist_2 = re.findall(
     r'(t/2016-a|/total-capital|t/overview-)', str(anchor_tag_box))
+# list(filter(r.match, str(anchor_tag_box)))
+print(newlist_2)
+
+# %%
+# 2107
+newlist = re.findall(
+    r'(2017App)', str(text_box))
+# list(filter(r.match, str(anchor_tag_box)))
+print(newlist)
+
+newlist_2 = re.findall(
+    r'(t/2017-a)', str(anchor_tag_box))
 # list(filter(r.match, str(anchor_tag_box)))
 print(newlist_2)
 
